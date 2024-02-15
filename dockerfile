853ca5868ffa1af9d0691c5b181ae48043788d83
kettleTeacues <teacues@gmail.com>
2024-02-15 07:02:43 +0000

    基礎実装を完了

--- dockerfile
+++ dockerfile
@@ -2,7 +2,7 @@ FROM ubuntu:22.04
 
 # 必要なパッケージをインストール
 RUN apt-get update && \
-    DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends nginx php-fpm mysql-server mysql-client php-mysql
+    DEBIAN_FRONTEND=noninteractive apt-get install -y nginx php php-fpm php-mysql mysql-server mysql-client
 
 # タイムゾーンを設定
 RUN ln -sf /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
@@ -12,26 +12,28 @@ RUN apt-get install -y wget && \
     wget https://wordpress.org/latest.tar.gz && \
     tar -xvzf latest.tar.gz && \
     rm latest.tar.gz && \
-    mv wordpress /var/www/html/ && \
-    mv /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
-
-# パーミッションを設定
-RUN chown -R www-data:www-data /var/www/html/wordpress && \
-    chmod -R 755 /var/www/html/wordpress
+    mv wordpress /var/www/ && \
+    mv /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php
 
 # configファイルをコピー
 COPY nginx/nginx.conf /etc/nginx/sites-available/default
 COPY php-fpm/php.ini /etc/php/8.1/fpm/php.ini
 COPY wordpress/info.php /var/www/wordpress/info.php
-COPY wordpress/wp-config.php /var/www/html/wordpress/wp-config.php
+COPY wordpress/wp-config.php /var/www/wordpress/wp-config.php
+
+RUN service mysql start && \
+    mysql -e "CREATE DATABASE wordpress CHARACTER SET UTF8 COLLATE UTF8_BIN;" && \
+    mysql -e "CREATE USER 'wp_user'@'localhost' IDENTIFIED BY 'wp_user';" && \
+    mysql -e "GRANT ALL ON wordpress.* to 'wp_user'@'localhost';"
 
 # apache2を削除
 RUN apt remove apache2 -y && \
     apt purge apache2 -y && \
     rm -rf /etc/apache2
 
-# ポート80を公開
-EXPOSE 80
+# パーミッションを設定
+RUN chown -R www-data:www-data /var/www/wordpress && \
+    chmod -R 755 /var/www/wordpress
 
 # nginxとphp-fpmのサービスを起動
-CMD service nginx start && service php8.1-fpm start && tail -f /dev/null
+CMD service nginx start && service mysql start && service php8.1-fpm start && tail -f /dev/null
--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -1,3 +1,23 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the installation.
+ * You don't have to use the web site, you can copy this file to "wp-config.php"
+ * and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * Database settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://wordpress.org/documentation/article/editing-wp-config-php/
+ *
+ * @package WordPress
+ */
+
 // ** データベース設定 - この情報はホスティング先から入手してください。 ** //
 /** WordPress のためのデータベース名 */
 define( 'DB_NAME', 'wordpress' );
@@ -9,10 +29,70 @@ define( 'DB_USER', 'wp_user' );
 define( 'DB_PASSWORD', 'wp_user' );
 
 /** データベースのホスト名 */
-define( 'DB_HOST', 'localhost' );
+// 単一のdockerコンテナで動かしているがlocalhostでは接続エラーが発生する。
+// 127.0.0.1 の場合はOK
+define( 'DB_HOST', '127.0.0.1' );
 
 /** データベースのテーブルを作成する際のデータベースの文字セット */
 define( 'DB_CHARSET', 'utf8' );
 
 /** データベースの照合順序 (ほとんどの場合変更する必要はありません) */
 define( 'DB_COLLATE', '' );
+
+/**#@+
+ * Authentication unique keys and salts.
+ *
+ * Change these to different unique phrases! You can generate these using
+ * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
+ *
+ * You can change these at any point in time to invalidate all existing cookies.
+ * This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         'put your unique phrase here' );
+define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
+define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
+define( 'NONCE_KEY',        'put your unique phrase here' );
+define( 'AUTH_SALT',        'put your unique phrase here' );
+define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
+define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
+define( 'NONCE_SALT',       'put your unique phrase here' );
+
+/**#@-*/
+
+/**
+ * WordPress database table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://wordpress.org/documentation/article/debugging-in-wordpress/
+ */
+define( 'WP_DEBUG', false );
+
+/* Add any custom values between this line and the "stop editing" line. */
+
+
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
